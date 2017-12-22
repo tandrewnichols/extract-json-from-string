c37ad5951a453a0158cad7559b15f71d6dd65578
Andrew Nichols <tandrewnichols@gmail.com>
2017-12-22 11:25:27 -0500

    Add codeclimate token and set coverage collection to run on v8

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var codeclimate = require('gulp-codeclimate-reporter');
 
 gulp.task('codeclimate', function() {
-  if (process.version.indexOf('v4') > -1) {
+  if (process.version.indexOf('v8') > -1) {
     gulp.src('coverage/lcov.info', { read: false })
       .pipe(codeclimate({
-        token: ''
+        token: process.env.CODECLIMATE_REPO_TOKEN
       }));
   }
 });
