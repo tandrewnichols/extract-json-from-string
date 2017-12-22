6d49a1116d1fc4fb3dc01191f4ab8dd7de93e03b
Andrew Nichols <tandrewnichols@gmail.com>
2017-12-22 16:44:19 -0500

    Replace gulp-codeclimate-reporter with one that doesn't end the stream

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -3,10 +3,10 @@ const codeclimate = require('gulp-codeclimate-reporter');
 
 gulp.task('codeclimate', (done) => {
   if (process.version.indexOf('v8') > -1) {
-    gulp.src('coverage/lcov.info', { read: false })
+    return gulp.src('coverage/lcov.info', { read: false })
       .pipe(codeclimate({
         token: process.env.CODECLIMATE_REPO_TOKEN
-      })).on('finish', () => done());
+      }));
   } else {
     done();
   }
